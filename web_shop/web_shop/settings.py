bfe4ec117fb039f163426a2f6668916022b660eb
SunPower120 <112656928+SunPower120@users.noreply.github.com>
2023-08-20 17:49:16 +0300

    Added Dockerand SQL

--- web_shop/web_shop/settings.py
+++ web_shop/web_shop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'web_shop_main',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,14 @@ WSGI_APPLICATION = 'web_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webshop_db',
+        'USER': 'webshop_user',
+        'PASSWORD': 'webshop_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
