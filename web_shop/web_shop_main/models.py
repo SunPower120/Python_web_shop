e7bfc1e9ac507ced1eca7d6b38c51565b035ac34
SunPower120 <112656928+SunPower120@users.noreply.github.com>
2023-08-20 21:11:57 +0300

    Added User Profiles

--- web_shop/web_shop_main/models.py
+++ web_shop/web_shop_main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +9,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_shop_owner = models.BooleanField(default=False)
